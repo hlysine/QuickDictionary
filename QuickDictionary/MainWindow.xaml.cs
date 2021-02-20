93f9585057518a8336ead580c9c2a1552a9a2055
Henry Lin <henry.ys.lin@gmail.com>
2021-02-20 20:50:01 +0800

    Also grab Chinese translation for Cambridge dictionary

--- QuickDictionary/MainWindow.xaml.cs
+++ QuickDictionary/MainWindow.xaml.cs
@@ -1154,7 +1154,8 @@ namespace QuickDictionary
             },
             GetDescription = async (browser) =>
             {
-                return await browser.GetInnerTextByXPath(@"//div[contains(@class,""def ddef_d"")]");
+                return await browser.GetInnerTextByXPath(@"//div[contains(@class,""def ddef_d"")]") + Environment.NewLine +
+                    await browser.GetInnerTextByXPath(@"//span[contains(@class,""trans dtrans"")]");
             },
             Icon = PackIconKind.LetterCBox,
             Name = "Cambridge English-Chinese Dictionary",
